b3197196b7956bd815f2ca5b2be99fd22cd5431a
Vladislav Ovcharenko <vladislav.ovcharenko110@gmail.com>
2020-10-31 13:15:25 +0200

    PR5

---
+++ assets/js/slider.js
@@ -0,0 +1,92 @@
+var slideIndex = 0;
+var currentSlideIndex = 0;
+var slideArray = [];
+
+function Slide(title, subtitle, background, link ) {
+   this.title = title;
+   this.subtitle = subtitle;
+   this.background = background;
+   this.link = link;
+   this.id = "slide" + slideIndex;
+   slideIndex++;
+   slideArray.push(this);
+}
+
+// ви можете зробити скільки завгодно слайдів
+
+var slide1 = new Slide(
+   "MCDonalds",
+   "Welcome to the official McDonald's Ukraine website. Learn more about our menu, promotions, and more.",
+   "public/images/mc.jpg",
+   "https://www.mcdonalds.com/"
+);
+
+
+var slide2 = new Slide(
+   "KFC",
+   "KFC gives you an opportunity to reveal the secret of recipe of 11 herbs and spices, to see how we create our unique chicken breading and to learn how to wash things right.",
+   "public/images/KFC.jpg",
+   "https://www.kfc-ukraine.com/"
+);
+
+var slide3 = new Slide(
+   "Starbucks",
+   "Join Starbucks® Rewards to enter a magical, starry world. Play Starbucks® Rewards Starland for your chance to win dreamy prizes like Bonus Stars, free handcrafted drinks, breakfast and more.",
+   "public/images/starbucks.jpg",
+   "https://www.starbucks.com/"
+);
+
+
+
+function buildSlider(){
+   var myHTML;
+   for(var i = 0; i < slideArray.length; i++) {
+       myHTML += "<div id='" + slideArray[i].id +
+           "' class='singleSlide' style='background-image:url(" + slideArray[i].background + ");'>" +
+           "<div class='slideOverlay'>" +
+           "<h1>" + slideArray[i].title + "</h1>" +
+           "<h4>" + slideArray[i].subtitle + "</h4>" +
+           "<a class='slider' href='" + slideArray[i].link + "' target='_blank'>Open Link</a>" +
+           "</div>" +
+           "</div>";
+   }
+
+   document.getElementById("mySlider").innerHTML = myHTML;
+   document.getElementById("slide" + currentSlideIndex).style.left = 0;
+}
+
+buildSlider();
+
+function prevSlide(){
+   var nextSlideIndex;
+   if (currentSlideIndex === 0 ) {
+       nextSlideIndex = slideArray.length - 1;
+   } else {
+       nextSlideIndex = currentSlideIndex - 1;
+   }
+
+   document.getElementById("slide" + nextSlideIndex).style.left = "-100%";
+   document.getElementById("slide" + currentSlideIndex).style.left = 0;
+
+   document.getElementById("slide" + nextSlideIndex).setAttribute("class", "singleSlide slideInLeft");
+   document.getElementById("slide" + currentSlideIndex).setAttribute("class", "singleSlide slideOutRight");
+
+   currentSlideIndex = nextSlideIndex;
+}
+
+function nextSlide(){
+   var nextSlideIndex;
+   if (currentSlideIndex === (slideArray.length - 1) ) {
+       nextSlideIndex = 0;
+   } else {
+       nextSlideIndex = currentSlideIndex + 1;
+   }
+
+   document.getElementById("slide" + nextSlideIndex).style.left = "100%";
+   document.getElementById("slide" + currentSlideIndex).style.left = 0;
+
+   document.getElementById("slide" + nextSlideIndex).setAttribute("class", "singleSlide slideInRight");
+   document.getElementById("slide" + currentSlideIndex).setAttribute("class", "singleSlide slideOutLeft");
+
+   currentSlideIndex = nextSlideIndex;
+}
